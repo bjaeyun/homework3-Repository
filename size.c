14bc0652b7ed164bf4306d88b90d9e5cea893b73
bjaeyun <119185674+bjaeyun@users.noreply.github.com>
2023-04-04 23:44:30 +0900

    printf("----- [변재윤] [2022041046] -------\n\n");

--- size.c
+++ size.c
@@ -3,6 +3,7 @@
 void main()
 {
 int **x;
+printf("----- [변재윤] [2022041046] -------\n\n");
 printf("sizeof(x) = %lu\n", sizeof(x));
 printf("sizeof(*x) = %lu\n", sizeof(*x));
 printf("sizeof(**x) = %lu\n", sizeof(**x));
